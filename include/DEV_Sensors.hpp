677bfa08e98030ed978325123fefe2c4e8fef0b8
Oleksii Kutuzov <oleksii.kutuzov@icloud.com>
2022-09-10 16:36:42 +0200

    Add battery status characteristic

--- include/DEV_Sensors.hpp
+++ include/DEV_Sensors.hpp
@@ -9,28 +9,25 @@ struct DEV_TemperatureSensor : Service::TemperatureSensor { // A standalone Air 
 		temp = new Characteristic::CurrentTemperature(0, true);
 		temp->setRange(-50, 100);
 
+		LOG0("Configuring Temperature Sensor"); // initialization message
+		LOG0("\n");
+
 	} // end constructor
-
-	void loop() {
-
-	} // loop
 };
 
 struct DEV_HumiditySensor : Service::HumiditySensor { // A standalone Air Quality sensor
 
-	SpanCharacteristic *hum;
+	SpanCharacteristic *hum;     // reference to the Humidity Characteristic
+	SpanCharacteristic *battery; // reference to Low Battery Characteristic
 
 	DEV_HumiditySensor() : Service::HumiditySensor() { // constructor() method
 
 		hum = new Characteristic::CurrentRelativeHumidity(0, true);
 
-		Serial.print("Configuring Humidity Sensor"); // initialization message
-		Serial.print("\n");
+		battery = new Characteristic::StatusLowBattery();
+
+		LOG0("Configuring Humidity Sensor"); // initialization message
+		LOG0("\n");
 
 	} // end constructor
-
-	void loop() {
-	}
-
-	// } // loop
 };
